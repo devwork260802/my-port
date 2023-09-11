40417736d08a7bbb128558903ebafee36fb7ba7e
Dev <devwork260802@gmail.com>
2023-09-11 19:29:28 +0530

    new

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const routes = require('./src/routes/main')
 
 const app=express();
 const port=9000;
 
-// app.use('',routes);
-
 app.use("/",(req,res)=>{
     res.json({message: "Hello"});
 });
-
-app.set("views","views");
 
 app.listen(9000,()=>{
     console.log(`Starting Server on Port ${port}`);
