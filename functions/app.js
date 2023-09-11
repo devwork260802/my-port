1d1e5d39c4f3031bd314131f2e48ed9ac839ff41
Dev <devwork260802@gmail.com>
2023-09-11 21:35:20 +0530

    new

---
+++ functions/app.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const serverless=require("serverless-http");
+const app=express();
+const router=express.Router();
+
+router.get("/",(req,res)=>{
+    res.send("Hello")
+})
+
+app.use('/.netlify/functions/app',router)
+
+module.exports.handler=serverless(app);
