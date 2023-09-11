b0069243f6e2bcfee83c345ba6a318fcaf8e6959
Dev <devwork260802@gmail.com>
2023-09-11 18:09:37 +0530

    new

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app=express();
+const port=9000;
+
+app.use("/",(req,res)=>{
+    res.json({message: "Hello"});
+});
+
+app.listen(9000,()=>{
+    console.log(`Starting Server on Port ${port}`);
+});
